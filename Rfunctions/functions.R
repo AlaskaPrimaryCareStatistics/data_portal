486276beb96afb53b8ff22e96e795e131d2f9c04
jordyBernard406 <jordy.bernard.406@gmail.com>
2021-11-17 10:20:38 -0900

    backup 11/17/2021

--- Rfunctions/functions.R
+++ Rfunctions/functions.R
@@ -1,3 +1,5 @@
+# Need to document see_prop #
+# Used in plots 1, 3, and 5
 see_prop <- function(data, xvar, yvar, ovar, main=NA, xlab=NA, ylab=NA, dr=NA, legend=legend, lx, ly, ci="Yes", ylim){
   d1 <- dr[1]
   d2 <- dr[2]
@@ -77,45 +79,45 @@ see_prop <- function(data, xvar, yvar, ovar, main=NA, xlab=NA, ylab=NA, dr=NA, l
   lwd=4
   for (j in 1:ncol(p_mat)){
     if (j==1){
-      plot(1:nrow(p_mat), p_mat[,j], ylim=ylim, col=cols[j], xlab=xlab, ylab=ylab, main=main, xaxt="n")
+      plot(1:nrow(p_mat), p_mat[,j]*100, ylim=ylim, col=cols[j], xlab=xlab, ylab=ylab, main=main, xaxt="n")
       rect(par("usr")[1], par("usr")[3],
            par("usr")[2], par("usr")[4],
            col = "grey10") # Color
       lines(c(-1, 50), c(0,0))
-      lines(c(-1, 50), c(0.10, 0.10))
-      lines(c(-1, 50), c(0.20, 0.20))
-      lines(c(-1, 50), c(0.30, 0.30))
-      lines(c(-1, 50), c(0.40, 0.40))
-      lines(c(-1, 50), c(0.50, 0.50))
-      lines(c(-1, 50), c(0.60, 0.60))
-      lines(c(-1, 50), c(0.70, 0.70))
-      lines(c(-1, 50), c(0.80, 0.80))
-      lines(c(-1, 50), c(0.90, 0.90))
-      lines(c(-1, 50), c(1, 1))
+      # lines(c(-1, 50), c(1, 1))
+      # lines(c(-1, 50), c(2, 2))
+      # lines(c(-1, 50), c(3, 0.30))
+      # lines(c(-1, 50), c(0.40, 0.40))
+      # lines(c(-1, 50), c(0.50, 0.50))
+      # lines(c(-1, 50), c(0.60, 0.60))
+      # lines(c(-1, 50), c(0.70, 0.70))
+      # lines(c(-1, 50), c(0.80, 0.80))
+      # lines(c(-1, 50), c(0.90, 0.90))
+      lines(c(-1, 50), c(100, 100))
       axis(1, at=1:length(dates), dates)
       if (!isTRUE(ci)){
-        lines(1:nrow(p_mat), p_mat[,j], col=cols[j], lwd=2)
+        lines(1:nrow(p_mat), p_mat[,j]*100, col=cols[j], lwd=2)
       }else{
         x <- c(1:nrow(p_mat), nrow(p_mat):1)
         y <- c(p_lo_mat[,j], rev(p_hi_mat[,j]))
         y[y < 0] <- 0
         polygon(
           x = x,
-          y = y,
+          y = y*100,
           col = adjustcolor(cols[j], alpha.f=0.5),
           border = NA
         )
       }
     }else{
       if(!isTRUE(ci)){
-        lines(1:nrow(p_mat), p_mat[,j], col=cols[j], lwd=2)
+        lines(1:nrow(p_mat), p_mat[,j]*100, col=cols[j], lwd=2)
       }else {
         x = c(1:nrow(p_mat), nrow(p_mat):1)
         y = c(p_lo_mat[,j], rev(p_hi_mat[,j]))
         y[y < 0] <- 0
         polygon(
           x = x,
-          y = y,
+          y = y*100,
           col = adjustcolor(cols[j], alpha.f=0.5),
           border = NA
         )
@@ -132,5 +134,188 @@ see_prop <- function(data, xvar, yvar, ovar, main=NA, xlab=NA, ylab=NA, dr=NA, l
       lty=1,
       text.col=cols[1:length(name_v)]
     )
+  }
+}
+
+
+get_month <- function(date){
+  as.numeric(as.character(date, format="%m"))
+}
+
+get_year <- function(date){
+  as.numeric(as.character(date, format="%Y"))
+}
+
+# goof around with some survival analysis #
+#' @param times vector of encounter times
+#' @param np number of patients seen
+surv_fun <- function(times, np){
+  df <- data.frame(
+    time = 1:max(times),
+    cov = rep(NA, length(1:max(times))),
+    sur = rep(NA, length(1:max(times))),
+    n = rep(NA, length(1:max(times)))
+  )
+  for (t in 1:max(times)){
+    df$cov[t] <- sum(na.omit(times==t))
+  }
+  for (t in max(times):1){
+    df$sur[t] <- (np-sum(df$cov[1:t]))/np
+    df$n[t] <- np - sum(df$cov[1:t])
+  }
+  return(df)
+}
+
+# Existing code #
+#' @param times 
+#' list of vectors; 
+#'    each vector stores encounter times; 
+#'    names(list) = variable names over which data is stratified
+#' @param nps
+#' (vector) number of patients associated with stratification variables  
+plot_surv <- function(times, nps, dates, var, ylim, lx, leg, ci, main){
+  for (i in 1:length(times)){
+    sf <- surv_fun(times[[i]], nps[i])
+    cols <- c("red", "green", "pink", "yellow", "#FF8000", "#80FF00", "cyan", "#0080FF", "#8000FF", "magenta", "#FF0080","blue")
+    lwd=4
+    if (i==1){
+      plot(sf[,1], sf[,3], ty="l", col=cols, ylim=ylim, xlab=NA, ylab="Percentage", xaxt="n", main=main)
+      rect(par("usr")[1], par("usr")[3],
+           par("usr")[2], par("usr")[4],
+           col = "grey10") # Color
+      lines(c(-1, 50), c(0,0))
+      # lines(c(-1, 50), c(0.10, 0.10))
+      # lines(c(-1, 50), c(0.20, 0.20))
+      # lines(c(-1, 50), c(0.30, 0.30))
+      # lines(c(-1, 50), c(0.40, 0.40))
+      # lines(c(-1, 50), c(0.50, 0.50))
+      # lines(c(-1, 50), c(0.60, 0.60))
+      # lines(c(-1, 50), c(0.70, 0.70))
+      # lines(c(-1, 50), c(0.80, 0.80))
+      # lines(c(-1, 50), c(0.90, 0.90))
+      lines(c(-1, 50), c(100, 100))
+      axis(1, at=1:length(dates), dates)
+      if (ci == "Point Estimate"){
+        lines(sf[,1], sf[,3]*100, col=cols[i], lwd=lwd)
+      }else{
+        sf$n[sf$n > 60000] <- 60000
+        se <- sf$sur*sqrt(sum(sf$cov/(sf$n*(sf$n-sf$cov))))
+        lo <- sf$sur-1.96*se
+        hi <- sf$sur+1.96*se
+        x = c(1:length(sf[,1]), length(sf[,1]):1)
+        y = c(lo, rev(hi))
+        y[y > 1] <- 1
+        polygon(
+          x = x,
+          y = y*100,
+          col = adjustcolor(cols[i], alpha.f=0.5),
+          border = NA
+        )
+      }
+      name_v <- names(times)
+      if (isTRUE(leg)){
+        legend(
+          lx, 
+          ly,
+          bg="grey10",
+          name_v,
+          cex = 1.25,
+          col=cols[1:length(name_v)],
+          lty=1,
+          lwd=4,
+          text.col=cols[1:length(name_v)]
+        )
+      }
+    }else{
+      if (ci=="Point Estimate"){
+        lines(sf[,1], sf[,3]*100, ty="l", col=cols[i], lwd=lwd)
+      }else{
+        sf$n[sf$n > 60000] <- 60000
+        se <- sf$sur*sqrt(sum(sf$cov/(sf$n*(sf$n-sf$cov))))
+        lo <- sf$sur-1.96*se
+        hi <- sf$sur+1.96*se
+        x = c(1:length(sf[,1]), length(sf[,1]):1)
+        y = c(lo, rev(hi))
+        y[y > 1] <- 1
+        polygon(
+          x = x,
+          y = y*100,
+          col = adjustcolor(cols[i], alpha.f=0.5),
+          border = NA
+        )
+      }
+    }
+  }
+}
+
+#' @param data main data table
+#' @param xvar x-axis variable -- will generally be time
+#' @param yvar y-axis variable -- e.g., covid or vaccinated
+#' @param ovar user input stratification variable
+#' @param dr user input daterange
+#' @param ylim user input y range
+survival_plot <- function(data, xvar, yvar, ovar, dr, ylim, ci, main){
+  if (ovar == "Health Center"){
+    ovar <- "center"
+  }
+  if (ovar == "Age"){
+    ovar <- "age"
+    legend <- "Y"
+  }
+  if (ovar == "Sex"){
+    ovar <- "sex"
+    legend <- "Y"
+  }
+  if (ovar == "Race"){
+    ovar <- "race"
+    legend <- "Y"
+  }
+  if (ovar == "Ethnicity"){
+    ovar <- "ethnicity"
+    legend <- "Y"
+  }
+  if (ovar == "Language"){
+    ovar <- "language"
+    legend <- "Y"
+  }
+  if (ovar == "Insurance Class"){
+    ovar <- "insurance"
+    legend <- "Y"
+  }
+  if (ovar == "Alcohol Use Disorder"){
+    ovar <- "alcohol"
+    legend <- "Y"
+  }
+  if (ovar == "Depression Diagnosis"){
+    ovar <- "depression"
+    legend <- "Y"
+  }
+  if (ovar == "Tobacco Use"){
+    ovar <- "tobacco"
+    legend <- "Y"
+  }
+  d <- data[!is.na(data[,ovar]),]
+  lev <- levels(d[,ovar])
+  nps <- rep(NA, length(lev))
+  for (i in 1:length(lev)){
+    nps[i] <- length(unique(d$mrn[d[,ovar]==lev[i]]))
+  }
+  times <- list()
+  d <- d[d[,which(names(d)==yvar)] == "Y",]
+  for (i in 1:length(lev)){
+    times[[i]] <- d$time[d[,ovar]==lev[i]]
+  }
+  names(times) <- lev
+  # extract dates #
+  d1 <- dr[1]
+  d2 <- dr[2]
+  d1 <- as.POSIXct(d1, format="%y-%m-%d")
+  d2 <- as.POSIXct(d2, format="%y-%m-%d")
+  data <- data[data$date >= d1 & data$date <= d2,]
+  dates <- unique(as.character(sort(data$date), format="%m/%Y"))
+  if (ovar=="center"){
+    plot_surv(times, nps, dates, ovar, ylim=ylim, lx="bottomleft", leg=F, ci=ci, main=main)
+  }else{
+    plot_surv(times, nps, dates, ovar, ylim=ylim, lx="bottomleft", leg=T, ci=ci, main=main)
   }
 }
--- app.R
+++ app.R
@@ -1,9 +1,9 @@
 
-library(shiny)
-library(shinydashboard)
-library(leaflet)
-
+#### Load Libraries and Helper Functions #####
 {
+    library(shiny)
+    library(shinydashboard)
+    library(leaflet)
     setwd("Rfunctions")
     for (file in list.files()){
         source(file)
@@ -15,36 +15,71 @@ library(leaflet)
     setwd("../")
 }
 
-strat_choices <- c("Health Center","Age","Sex","Race","Ethnicity","Language","Insurance Class","Alcohol Use Disorder","Depression Diagnosis","Tobacco Use")
+##### Global Variables #####
+{
+    strat_choices <- c("Age","Sex","Race","Ethnicity","Language","Insurance Class","Alcohol Use Disorder","Depression Diagnosis","Tobacco Use")
+}
 
-# Define UI for application that draws a histogram
+##### Set Up User Interface #####
 ui <- dashboardPage(
+    
+    ### Header ###
     dashboardHeader(title = "APCA Data Portal"),
+    
+    ### Side Bar ###
     dashboardSidebar(
         sidebarMenu(
-            menuItem("About", tabName = "tab1"),
+            # menuItem("About", tabName = "tab1"),
             menuItem("Covid-19 Infections", tabName = "tab2"),
             menuItem("Covid-19 Vaccinations", tabName = "tab3"),
             menuItem("Telehealth Utilization", tabName = "tab4")
         )
     ),
+    
+    ### Body ###
     dashboardBody(
         tabItems(
-            tabItem(
-                tabName = "tab1",
-                h3("Welcome to the Alaska Primary Care Associations Data Portal"),
-                m <- leaflet() %>%
-                    addTiles() %>%
-                    addMarkers(data = mapData, lng = ~lon, lat = ~lat, popup = ~center),
-                url_1 <- a("APCA Website", href="https://alaskapca.org/"),
-                url_2 <- a("APCA GitHub Page", href="https://github.com/AlaskaPrimaryCareStatistics")
-            ),
+            
+            # ### Tab 1 --> Landing Page ###
+            # tabItem(
+            #     tabName = "tab1",
+            #     h3("Welcome to the Alaska Primary Care Associations Data Portal"),
+            #     h2("About"),
+            #     m <- leaflet() %>%
+            #         addTiles() %>%
+            #         addMarkers(data = mapData, lng = ~lon, lat = ~lat, popup = ~center),
+            #     url_1 <- a("APCA Website", href="https://alaskapca.org/"),
+            #     url_2 <- a("APCA GitHub Page", href="https://github.com/AlaskaPrimaryCareStatistics")
+            # ),
+            
+            
+            
+            ### Tab 2 --> Covid-19 Infections ###
             tabItem(
                 tabName = "tab2",
+                # Big Button 1#
                 div(
-                    style="padding: 10px; font-size:20px;",
-                    strong("About:")
+                  
+                  style = "text-align: center; border: 1px solid grey; padding: 10px; background-color: rgb(211,211,211);",
+                  div(
+                    style="font-size:20px; padding: 0px; ",
+                    strong("Visualize:")
+                  ),
+                  splitLayout(
+                    radioButtons(
+                      inputId = "surv_cov",
+                      label = NULL,
+                      choices = c(
+                        "Percent of Patients Diagnosed with New Case of Covid-19 During Encounter", 
+                        "Percent of Patients without Positive Covid-19 Diagnosis on Record"
+                        ),
+                      inline = F,
+                      width = "100%"
+                    )
+                  )
                 ),
+                
+                # Big Button 2#
                 div(
                     style = "text-align: center; border: 1px solid grey; padding: 10px; background-color: rgb(211,211,211);",
                     div(
@@ -60,28 +95,12 @@ ui <- dashboardPage(
                         )
                     )
                 ),
+                
+                # Button Row 2 Panel 1 #
                 style = "padding: 0px;",
                 splitLayout(
                     style = "padding: 0px;",
-                    cellWidths = c("30%", "39.3%", "30%"),
-                    tags$div(
-                        style = "text-align: center; border: 1px solid grey; background-color: rgb(211,211,211);",
-                        div(
-                            style="font-size:20px;  padding-top: 10px; padding-bottom: 0px;",
-                            strong("Y Axis Limits")
-                        ),
-                        div(
-                            style="padding-left: 10px; padding-right: 10px;",
-                            sliderInput(
-                                inputId = "y_cov",
-                                min = 0, 
-                                max = 1,
-                                value = c(0, 0.1),
-                                label = NULL,
-                                width="100%"
-                            )
-                        )
-                    ),
+                    cellWidths = c("49.75%", "49.75%"),
                     tags$div(
                         style="text-align: center; border: 1px solid grey; background-color: rgb(211,211,211);",
                         div(
@@ -89,7 +108,7 @@ ui <- dashboardPage(
                             strong("Date Range:")
                         ),
                         tags$div(
-                            style="padding-left: 10px; padding-right: 10px;",
+                            style="padding-left: 10px; padding-right: 40px;",
                             sliderInput("dr_cov",
                                         label=NULL,
                                         min = min(data_covid$date),
@@ -116,25 +135,43 @@ ui <- dashboardPage(
                         )
                     )
                 ),
+                
+                # Plot pannel 1 #
                 div(
                     style = "border: 1px solid grey; padding:20px; background-color: rgb(255,255,255);",
                     plotOutput(
                         outputId = "p21"
                     )
-                ),
-                div(
-                    style = "border: 1px solid grey; padding:20px; background-color: rgb(255,255,255);",
-                    plotOutput(
-                        outputId = "p22"
-                    )
                 )
+                # ,
+                # tags$a("Click here to download the full report", href="report.pdf")
             ),
+            
+            ### Tab 3 --> Covid-19 Vaccinations ###
             tabItem(
                 tabName = "tab3",
+                # Big Button 1#
                 div(
-                    style="padding: 10px; font-size:20px;",
-                    strong("About:")
+                  style = "text-align: center; border: 1px solid grey; padding: 10px; background-color: rgb(211,211,211);",
+                  div(
+                    style="font-size:20px; padding: 0px; ",
+                    strong("Visualize:")
+                  ),
+                  splitLayout(
+                    radioButtons(
+                      inputId = "surv_vac",
+                      label = NULL,
+                      choices = c(
+                        "Percent of Patients Receiving Covid-19 Vaccination During Encounter", 
+                        "Percent of Patients without CHC Administered Covid-19 Vaccine "
+                      ),
+                      inline = F,
+                      width = "100%"
+                    )
+                  )
                 ),
+                
+                # Big Button Panel 1#
                 div(
                     style = "text-align: center; border: 1px solid grey; padding: 10px; background-color: rgb(211,211,211);",
                     div(
@@ -150,28 +187,12 @@ ui <- dashboardPage(
                         )
                     )
                 ),
+                
+                # Button Row 2 Panel 1#
                 style = "padding: 0px;",
                 splitLayout(
                     style = "padding: 0px;",
-                    cellWidths = c("30%", "39.3%", "30%"),
-                    tags$div(
-                        style = "text-align: center; border: 1px solid grey; background-color: rgb(211,211,211);",
-                        div(
-                            style="font-size:20px;  padding-top: 10px; padding-bottom: 0px;",
-                            strong("Y Axis Limits")
-                        ),
-                        div(
-                            style="padding-left: 10px; padding-right: 10px;",
-                            sliderInput(
-                                inputId = "y_vac",
-                                min = 0, 
-                                max = 1,
-                                value = c(0, 0.4),
-                                label = NULL,
-                                width="100%"
-                            )
-                        )
-                    ),
+                    cellWidths = c("49.75%", "49.75%"),
                     tags$div(
                         style="text-align: center; border: 1px solid grey; background-color: rgb(211,211,211);",
                         div(
@@ -179,7 +200,7 @@ ui <- dashboardPage(
                             strong("Date Range:")
                         ),
                         tags$div(
-                            style="padding-left: 10px; padding-right: 10px;",
+                            style="padding-left: 10px; padding-right: 40px;",
                             sliderInput("dr_vac",
                                         label=NULL,
                                         min = min(data_vaccination$date),
@@ -206,25 +227,20 @@ ui <- dashboardPage(
                         )
                     )
                 ),
+                # Plotting output #
                 div(
                     style = "border: 1px solid grey; padding:20px; background-color: rgb(255,255,255);",
                     plotOutput(
                         outputId = "p31"
                     )
-                ),
-                div(
-                    style = "border: 1px solid grey; padding:20px; background-color: rgb(255,255,255);",
-                    plotOutput(
-                        outputId = "p32"
-                    )
                 )
+                # ,
+                # tags$a("Click here to download the full report", href="report.pdf")
             ),
+            ### Tab 4 --> Covid-19 Vaccinations ###
             tabItem(
                 tabName = "tab4",
-                div(
-                    style="padding: 10px; font-size:20px;",
-                    strong("About:")
-                ),
+                # Big Button #
                 div(
                     style = "text-align: center; border: 1px solid grey; padding: 10px; background-color: rgb(211,211,211);",
                     div(
@@ -240,10 +256,11 @@ ui <- dashboardPage(
                         )
                     )
                 ),
+                # Button Row 2 #
                 style = "padding: 0px;",
                 splitLayout(
                     style = "padding: 0px;",
-                    cellWidths = c("30%", "39.3%", "30%"),
+                    cellWidths = c("30%", "39.2%", "30%"),
                     tags$div(
                         style = "text-align: center; border: 1px solid grey; background-color: rgb(211,211,211);",
                         div(
@@ -255,8 +272,8 @@ ui <- dashboardPage(
                             sliderInput(
                                 inputId = "y_tel",
                                 min = 0, 
-                                max = 1,
-                                value = c(0, 0.8),
+                                max = 100,
+                                value = c(0, 80),
                                 label = NULL,
                                 width="100%"
                             )
@@ -296,51 +313,71 @@ ui <- dashboardPage(
                         )
                     )
                 ),
+                
+                # Plotting Output #
                 div(
                     style = "border: 1px solid grey; padding:20px; background-color: rgb(255,255,255);",
                     plotOutput(
                         outputId = "p41"
                     )
                 )
+                # ,
+                # tags$a("Click here to download the full report", href="report.pdf")
             )
         )    
     )
 )
 
-# Define server logic required to draw a histogram
+##### SERVER LOGIC #####
 server <- function(input, output) {
+  
+    
+  
+    # tab 2 --> Covid --> plot 1 #
     output$p21 <- renderPlot(
         {
+          if (input$surv_cov=="Percent of Patients Diagnosed with New Case of Covid-19 During Encounter"){
             see_prop(
-                data = data_covid,
-                xvar = "time", 
-                yvar = "covid",
-                ovar = input$str_cov,
-                main = "Percentage of Patients Diagnosed with New Case Covid-19 During Encounter",
-                xlab = NA,
-                ylab = "Percentage",
-                dr = input$dr_cov,
-                legend = "N",
-                lx = 15,
-                ly = 1,
-                ci = input$ci_cov,
-                ylim = input$y_cov
+              data = data_covid,
+              xvar = "time", 
+              yvar = "covid",
+              ovar = input$str_cov,
+              main = input$surv_cov,
+              xlab = NA,
+              ylab = "Percentage",
+              dr = input$dr_cov,
+              legend = "N",
+              lx = 15,
+              ly = 1,
+              ci = input$ci_cov,
+              ylim = c(0,0.1)*100
             )
+          }else{
+            
+            survival_plot(
+              data = data_covid,
+              xvar = "time",
+              yvar = "covid",
+              ovar = input$str_cov,
+              dr = input$dr_cov, 
+              ylim = c(0.9,1)*100,
+              input$ci_cov,
+              main = input$surv_cov
+            )
+          }
         }
     )
-    output$p22 <- renderPlot(
-        {
-            plot(1:10, 1:10)
-        }
-    )
+    
+    # tab 3 --> Vaccination --> plot 1 #
     output$p31 <- renderPlot(
         {
+          if (input$surv_vac=="Percent of Patients Receiving Covid-19 Vaccination During Encounter"){
             see_prop(
                 data = data_vaccination,
                 xvar = "time", 
                 yvar = "vaccinated",
                 ovar = input$str_vac,
-                main = "Percentage of Encounters where Patient was Vaccinated by CHC",
+                main = input$surv_vac,
                 xlab = NA,
                 ylab = "Percentage",
                 dr = input$dr_vac,
@@ -348,15 +385,24 @@ server <- function(input, output) {
                 lx = 15,
                 ly = 1,
                 ci = input$ci_vac,
-                ylim = input$y_vac
+                ylim = c(0,0.3)*100
             )
+          }else{
+              survival_plot(
+                data = data_vaccination,
+                xvar = "time",
+                yvar = "vaccinated",
+                ovar = input$str_vac,
+                dr = input$dr_vac, 
+                ylim = c(0.6,1)*100,
+                ci = input$ci_vac,
+                main = input$surv_cov
+              )
+          }
         }
     )
-    output$p32 <- renderPlot(
-        {
-            plot(1:10, 1:10)
-        }
-    )
+    
+    # tab 4 --> Telehealth --> plot 1 #
     output$p41 <- renderPlot(
         {
             see_prop(
@@ -364,7 +410,7 @@ server <- function(input, output) {
                 xvar = "time", 
                 yvar = "telehealth",
                 ovar = input$str_tel,
-                main = "Percentage of Patient Encounters Occurring by Telehealth",
+                main = "Percentage of Patient Encounters in Occurring by Telehealth",
                 xlab = NA,
                 ylab = "Percentage",
                 dr = input$dr_tel,
@@ -376,6 +422,7 @@ server <- function(input, output) {
             )
         }
     )
+    
 }
 
 # Run the application 
